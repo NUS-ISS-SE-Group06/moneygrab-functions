254dad366b70aab8bc4af7c852b6b7e671934b0b
sivabalaji1986 <42265473+sivabalaji1986@users.noreply.github.com>
2025-06-22 23:47:23 +0800

    Created a Auth Constant file with Access Matrix

---
+++ auth-lambda/src/main/java/com/moola/fx/moneychanger/auth/constants/AuthConstant.java
@@ -0,0 +1,59 @@
+package com.moola.fx.moneychanger.auth.constants;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class AuthConstant {
+
+    // Cognito settings
+    public static final String ISSUER    =
+            "https://cognito-idp.ap-southeast-1.amazonaws.com/ap-southeast-1_XI7LG9T1f";
+
+    public static final String JWKS_URL = "/.well-known/jwks.json";
+
+    public static final String CLIENT_ID = "4lcfgcumiu5derbjmd2fm1io34";
+
+    public static final String AUTHORIZATION_HEADER = "authorization";
+
+    public static final String BEARER_REGEX = "(?i)^Bearer\\s+";
+
+    public static final String IS_AUTHORIZED_KEY = "isAuthorized";
+
+    public static final String COGNITO_GROUPS = "cognito:groups";
+
+    public static final Map<String,List<String>> ACCESS_MATRIX;
+    static {
+        Map<String,List<String>> m = new HashMap<>();
+        // MONEY CHANGER OPERATIONS
+        m.put("GET#/api/v1/money-changers", List.of("MoneyGrabAdmin", "MoneyGrabStaff"));
+        m.put("POST#/api/v1/money-changers", List.of("MoneyGrabAdmin", "MoneyGrabStaff"));
+        m.put("PUT#/api/v1/money-changers/{id}", List.of("MoneyGrabAdmin", "MoneyGrabStaff"));
+        m.put("DELETE#/api/v1/money-changers/{id}", List.of("MoneyGrabAdmin", "MoneyGrabStaff"));
+
+        // SCHEMES
+        m.put("GET#/api/v1/schemes", List.of("MoneyGrabAdmin", "MoneyGrabStaff"));
+        m.put("POST#/api/v1/schemes", List.of("MoneyGrabAdmin", "MoneyGrabStaff"));
+        m.put("PUT#/api/v1/schemes/{id}", List.of("MoneyGrabAdmin", "MoneyGrabStaff"));
+        m.put("DELETE#/api/v1/schemes/{id}", List.of("MoneyGrabAdmin", "MoneyGrabStaff"));
+
+        // COMMISSION RATES
+        m.put("GET#/api/v1/commission-rates", List.of("MoneyGrabAdmin", "MoneyGrabStaff"));
+        m.put("POST#/api/v1/commission-rates", List.of("MoneyGrabAdmin", "MoneyGrabStaff"));
+        m.put("PUT#/api/v1/commission-rates/{id}", List.of("MoneyGrabAdmin", "MoneyGrabStaff"));
+        m.put("DELETE#/api/v1/commission-rates/{id}", List.of("MoneyGrabAdmin", "MoneyGrabStaff"));
+
+        // COMPANY COMMISSION SCHEMES
+        m.put("GET#/api/v1/company-commission-schemes", List.of("MoneyGrabAdmin", "MoneyGrabStaff"));
+        m.put("POST#/api/v1/company-commission-schemes", List.of("MoneyGrabAdmin", "MoneyGrabStaff"));
+        m.put("PUT#/api/v1/company-commission-schemes/{id}", List.of("MoneyGrabAdmin", "MoneyGrabStaff"));
+        m.put("DELETE#/api/v1/company-commission-schemes/{id}", List.of("MoneyGrabAdmin", "MoneyGrabStaff"));
+
+        // CURRENCIES
+        m.put("GET#/api/v1/currencies", List.of("MoneyChangerAdmin", "MoneyChangerStaff"));
+
+        ACCESS_MATRIX = Collections.unmodifiableMap(m);
+    }
+
+}
